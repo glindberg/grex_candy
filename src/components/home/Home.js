0ffa07784fd71316351a0875da0c6add82857f09
b3rg3t <berget_90db@hotmail.com>
2019-01-24 14:54:53 +0100

    create react app config

---
+++ src/components/home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () =>(
+    <section>
+        <div><p>Div1</p></div>
+        <div><p>Div2</p></div>
+    </section>
+)
+
+export default Home;
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = () =>(
+    <section>
+        <div><p>Div3</p></div>
+        <div><p>Div4</p></div>
+    </section>
+)
+
+export default Profile;
