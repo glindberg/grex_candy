b41652a3cac8a51037b69bc62e02c4ac207f7572
Tina Olsson <tina.taajn.olsson@gmail.com>
2019-02-04 14:14:11 +0100

    Landingpage update

---
+++ src/components/LandingPage/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const LandingP = styled.div`
+  background: linear-gradient(rgb(207, 226, 199), rgb(118, 135, 110));
+  color: rgb(248, 249, 247);
+  height: 100vh;
+  margin-top: 0;
+  margin-bottom: 0;
+
+
+`;
+
+export const Flex = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    max-width: 60%;
+    align-content: center;
+    margin: auto;
+
+    h1 {
+      margin-top: 0;
+    }
+
+    p {
+    }
+
+    button {
+      width: 7em;
+      margin-top: 5em;      
+    }
+`;
