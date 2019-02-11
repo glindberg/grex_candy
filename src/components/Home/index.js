0167546213424406a44a9ad61b064769fc4ee5b3
Tina Olsson <tina.taajn.olsson@gmail.com>
2019-02-11 14:50:26 +0100

    updated order of code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -56,80 +56,6 @@ class HomePage extends Component {
   }
 }
 
-// TESTAR ATT VISA ACTIVITIES
-class ActivitesBase extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activity: "",
-
-      loading: false,
-      activities: [],
-    };
-  }
-  componentDidMount() {
-    this.setState({ loading: true });
-    this.props.firebase.activities().on('value', snapshot => {
-      const activityObject = snapshot.val();
-      if (activityObject) {
-        // convert messages list from snapshot
-        const activityList = Object.keys(activityObject).map(key => ({
-          ...activityObject[key],
-          uid: key,
-        }));
-        this.setState({
-          activities: activityList,
-          loading: false
-        });
-      } else {
-        this.setState({ activities: null, loading: false });
-      }
-    });
-  }
-  componentWillUnmount() {
-    this.props.firebase.activities().off();
-  }
-  render() {
-    const { activities, loading } = this.state;
-    return (
-      <div>
-        {loading && <div>Loading activities...</div>}
-        {activities ? (
-          <ActivityList activities={activities} />
-        ) : (
-            <div>There are no activities ...</div>
-          )}
-
-      </div>
-    );
-  }
-}
-
-const Activities = withFirebase(ActivitesBase);
-
-
-const ActivityList = ({ activities }) => (
-  <ul>
-    {activities.map(activity => (
-      <ActivityItem key={activity.uid} activity={activity} />
-    ))} </ul>
-);
-
-const ActivityItem = ({ activity }) => (
-  <li>
-
-
-    <strong>{activity.userId}</strong> <br /> {activity.activity} - {activity.otheractivity}<br />
-    {activity.actlengthstart} - {activity.actlengthend}
-
-
-  </li>
-);
-
-
-// END
-
-
 class MessagesBase extends Component {
   constructor(props) {
     super(props);
@@ -359,6 +285,79 @@ class MessageItem extends Component {
 }
 
 const Messages = withFirebase(MessagesBase);
+
+
+// TESTAR ATT VISA ACTIVITIES
+class ActivitesBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activity: "",
+
+      loading: false,
+      activities: [],
+    };
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+    this.props.firebase.activities().on('value', snapshot => {
+      const activityObject = snapshot.val();
+      if (activityObject) {
+        // convert messages list from snapshot
+        const activityList = Object.keys(activityObject).map(key => ({
+          ...activityObject[key],
+          uid: key,
+        }));
+        this.setState({
+          activities: activityList,
+          loading: false
+        });
+      } else {
+        this.setState({ activities: null, loading: false });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.props.firebase.activities().off();
+  }
+  render() {
+    const { activities, loading } = this.state;
+    return (
+      <div>
+        {loading && <div>Loading activities...</div>}
+        {activities ? (
+          <ActivityList activities={activities} />
+        ) : (
+            <div>There are no activities ...</div>
+          )}
+
+      </div>
+    );
+  }
+}
+
+const Activities = withFirebase(ActivitesBase);
+
+
+const ActivityList = ({ activities }) => (
+  <ul>
+    {activities.map(activity => (
+      <ActivityItem key={activity.uid} activity={activity} />
+    ))} </ul>
+);
+
+const ActivityItem = ({ activity }) => (
+  <li>
+
+
+    <strong>{activity.userId}</strong> <br /> {activity.activity} - {activity.otheractivity}<br />
+    {activity.actlengthstart} - {activity.actlengthend}
+
+
+  </li>
+);
+
+// END
 
 const condition = authUser => !!authUser;
 
